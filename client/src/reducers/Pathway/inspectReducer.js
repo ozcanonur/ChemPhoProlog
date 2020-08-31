d2f341a5787ebe4355b9af1482ba136619b19d2d
ozcanonur <ozcanonur10@gmail.com>
2020-08-31 03:00:22 +0100

    Fixed pathselectlist redux bug

--- client/src/reducers/Pathway/inspectReducer.js
+++ client/src/reducers/Pathway/inspectReducer.js
@@ -1,7 +1,12 @@
 import uniqWith from 'lodash/uniqWith';
 
 export default (state = [], action) => {
-  const filterDuplicates = () => uniqWith([...state, action.payload], (x, y) => x === y);
+  const filterDuplicates = () =>
+    uniqWith([...state, action.payload], (x, y) => {
+      const xId = x[0];
+      const yId = y[0];
+      return xId === yId;
+    });
 
   switch (action.type) {
     case 'ADD_INSPECT_PATH':
